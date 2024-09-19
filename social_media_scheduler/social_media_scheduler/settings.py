0a087f95906105da53dea19429561cb088138e3e
root <root@ketzim.ketzim.com>
2024-09-19 17:28:54 +0000

    Insert mapa

--- social_media_scheduler/social_media_scheduler/settings.py
+++ social_media_scheduler/social_media_scheduler/settings.py
@@ -133,8 +133,13 @@ USE_I18N = True
 
 USE_TZ = True
 
+# CONFIG DO IFRAME
+X_FRAME_OPTIONS = 'ALLOWALL'
+X_FRAME_OPTIONS = 'SAMEORIGIN'
+X_FRAME_OPTIONS = 'ALLOW-FROM https://app.ketzim.com/'  # Altere para o domínio que irá embutir
 
-# CONFIGO DO CORS
+
+# CONFIG DO CORS
 CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
--- social_media_scheduler/webservice/views.py
+++ social_media_scheduler/webservice/views.py
@@ -1,6 +1,15 @@
-from django.http import JsonResponse, Http404
+from django.http import JsonResponse, Http404, HttpResponse
 from perfil.models import Perfil  # Importe o modelo Perfil
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
 import requests
+import folium
+import geopandas as gpd
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView, TemplateView
+from geopy.geocoders import Nominatim
+from .models import Localizacao
+from .forms import MapaForm
 import os
 from dotenv import load_dotenv
 
@@ -8,17 +17,95 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Defina seu token padrão aqui (ou obtenha-o de um arquivo de configuração)
-#TOKEN_PADRAO = os.getenv('TOKEN_SECRET')
-
 TOKEN_PADRAO = os.getenv('TOKEN_SECRET').strip()
+
+# Capturando as coordernadas
+def get_coordinates(endereco):
+    # Inicializando o geocoder
+    geolocator = Nominatim(user_agent="meu_app")
+
+    # Geocodificando o endereço
+    location = geolocator.geocode(endereco)
+
+    if location:
+        return location.latitude, location.longitude
+    else:
+        return None, None
+    
+# Atualiza as informações de coordenadas a partir do endereço do banco
+def atualizar_coordenadas(loc):
+    # Obtém o endereço do objeto
+    endereco = loc.endereco  # Supondo que o campo do endereço se chama 'endereco'
+    
+    # Obter as coordenadas
+    latitude, longitude = get_coordinates(endereco)
+    
+    # Atualiza a localização com as novas coordenadas
+    loc.latitude = latitude
+    loc.longitude = longitude
+    loc.save()  # Salva as alterações no banco de dados
+    if latitude is not None and longitude is not None:
+        print(f"Atualizado: {endereco} - Latitude: {latitude}, Longitude: {longitude}")
+    else:
+        print(f"Endereço não encontrado: {endereco}")
+
+
+#Metodos para views da página mapa
+@method_decorator(login_required, name='dispatch')
+class MapaListView(ListView):
+    model = Localizacao
+    template_name = 'list_mapas.html'
+    context_object_name = 'enderecos'
+    #success_url = reverse_lazy('list_mapas')
+    
+@method_decorator(login_required, name='dispatch')
+class MapaCreateView(CreateView):
+    model = Localizacao
+    form_class = MapaForm
+    template_name = 'cria_edita_mapa.html'
+    success_url = reverse_lazy('MapaListView')
+
+    def form_valid(self, form):
+        response = super().form_valid(form)  # Salva o objeto no banco de dados
+        atualizar_coordenadas(self.object)  # Chama a função passando o objeto recém-criado
+        return response
+
+@method_decorator(login_required, name='dispatch')
+class MapaUpdateView(UpdateView):
+    model = Localizacao
+    form_class = MapaForm
+    template_name = 'cria_edita_mapa.html'
+    success_url = reverse_lazy('MapaListView')
+    
+    def form_valid(self, form):
+        response = super().form_valid(form)  # Salva o objeto no banco de dados
+        atualizar_coordenadas(self.object)  # Chama a função passando o objeto recém-criado
+        return response
+
+@method_decorator(login_required, name='dispatch')
+class MapaDeleteView(DeleteView):
+    model = Localizacao
+    template_name = 'publicacao_confirm_delete.html'
+    success_url = reverse_lazy('MapaListView')
+    
+@method_decorator(login_required, name='dispatch')
+class MapasView(TemplateView):
+    template_name = 'mapas_criados.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Buscando cidades distintas
+        context['enderecos'] = Localizacao.objects.values('cidade').distinct()
+        
+        return context
 
 def get_instagram_info(request, perfil_nome):
     try:
         # Obtenha o token dos parâmetros da URL
-        Token = request.GET.get('Token').strip()
+        token = request.GET.get('Token').strip()
         
         # Validação do token
-        if Token != TOKEN_PADRAO:
+        if token != TOKEN_PADRAO:
             return JsonResponse({'error': 'Token de acesso ao Webservice inválido.'}, status=403)
         
         # Busque o perfil no banco de dados pelo nome e rede social (Instagram)
@@ -59,4 +146,74 @@ def get_instagram_info(request, perfil_nome):
     except Exception as e:
         # Qualquer outro erro inesperado
         return JsonResponse({'error': str(e)}, status=500)
+
+    
+def get_mapa(request, cidade):
+    # Atualiza as coordenadas das localizações
+    #atualizar_coordenadas()
+
+   
+    # Busca as localizações filtradas pelo estado
+    localizacoes = Localizacao.objects.filter(cidade=cidade.upper())  # Filtra pelo estado
+    
+    # Adicionando um print para verificar as localizações
+    for loc in localizacoes:
+        print(f"Endereço: {loc.endereco}, Latitude: {loc.latitude}, Longitude: {loc.longitude}")
+        
+    # Criando o mapa com o ponto inicial definido
+    mapa = folium.Map(location=[loc.latitude, loc.longitude], zoom_start=13)
+
+    popup_content = f'<div style="font-size: 14px; color: green; font-weight: bold;">Obra - Protege Piso</div>'
+    
+    # Adicionando camadas de estilo com atribuições
+    #folium.TileLayer('Stamen Terrain', attr='Map tiles by Stamen Design, under CC BY 3.0.').add_to(mapa)
+    #folium.TileLayer('Stamen Toner', attr='Map tiles by Stamen Design, under CC BY 3.0.').add_to(mapa)
+    #folium.TileLayer('Stamen Watercolor', attr='Map tiles by Stamen Design, under CC BY 3.0.').add_to(mapa)
+    #folium.TileLayer('CartoDB positron', attr='Map tiles by CartoDB').add_to(mapa)
+    #folium.TileLayer('CartoDB dark_matter', attr='Map tiles by CartoDB').add_to(mapa)
+    
+    folium.TileLayer(
+        tiles='https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+        attr='Tiles © Esri & the GIS User Community',
+        name='Esri Satellite'
+    ).add_to(mapa)
+    
+    # Adicionando controle de camadas
+    folium.LayerControl().add_to(mapa)
+
+    # Adicionando marcadores para cada localização
+    for loc in localizacoes:
+        popup_content = f'<div style="font-size: 14px; color: green; font-weight: bold;">Obra - Protege Piso {loc.estado}</div>'
+        if loc.latitude is not None and loc.longitude is not None:
+            # Adicionando um marcador no mapa
+            folium.Marker(
+                location=[loc.latitude, loc.longitude],
+                popup=folium.Popup(popup_content, max_width=200),
+                icon=folium.Icon(color='green')
+            ).add_to(mapa)
+        else:
+            print(f"Coordenadas não encontradas para: {loc.endereco}")
+
+ # Gerando o HTML do mapa
+    mapa_html = f'''
+    <div style="width: 100%; height: 80%; overflow: hidden;">
+        {mapa._repr_html_()}
+    </div>
+    <style>
+        html, body {{
+            margin: 0;
+            padding: 0;
+            width: 100%;
+            height: 100%;
+        }}
+
+        /* Media Query para telas menores */
+        @media (max-width: 768px) {{
+            div {{
+                height: 50vh; /* Ajusta a altura para 50% da viewport em telas menores */
+            }}
+        }}
+    </style>
+    '''
+    return HttpResponse(mapa_html, content_type='text/html')
 
